f065a3f327feddd8d5828e3941ff7cfe081d37b4
eliansamuel <eliansamuelsantos@gmail.com>
2021-11-05 21:29:26 -0300

    Adding content inside TesteFuncionario class

---
+++ src/bytebank_herdado/TesteFuncionario.java
@@ -0,0 +1,18 @@
+package bytebank_herdado;
+
+public class TesteFuncionario {
+	
+	public static void main(String[] args) {
+		
+		Funcionario funcionario1 = new Funcionario();
+		
+		funcionario1.setNome("Elian");
+		funcionario1.setCpfString("222.222.222-22");
+		funcionario1.setSalario(5000);
+		
+		System.out.println("O funcionário " + funcionario1.getNome()
+				+ " terá uma bonificação de R$ "
+				+ funcionario1.getBonificacao());
+		
+	}
+}
