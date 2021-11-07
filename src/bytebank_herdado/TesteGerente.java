81d4781f5c3ae76807d35a22178996609f32fdf8
eliansamuel <eliansamuelsantos@gmail.com>
2021-11-07 17:23:37 -0300

    Adding methods to the TesteGerente class

--- src/bytebank_herdado/TesteGerente.java
+++ src/bytebank_herdado/TesteGerente.java
@@ -3,8 +3,23 @@ package bytebank_herdado;
 public class TesteGerente {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Gerente g1 = new Gerente();
+		
+		g1.setNome("Marco");
+		g1.setCpfString("111.111.111-11");
+		g1.setSalario(5000);;
+		
+		System.out.println(g1.getNome());
+		System.out.println(g1.getCpfString());
+		System.out.println(g1.getSalario());
+		
+		g1.setSenha(2222);
+		boolean autenticou = g1.autentica(2222);
+		
+		System.out.println(autenticou);
+		
+		
+		
 	}
-
 }
