fcad923a406c381a4c09c282dc3ec94fd88cbd4e
eliansamuel <eliansamuelsantos@gmail.com>
2021-11-07 17:11:13 -0300

    Creating Gerente and TesteGerente classes

---
+++ src/bytebank_herdado/Gerente.java
@@ -0,0 +1,20 @@
+package bytebank_herdado;
+
+// Gerente eh um Funcionario, Gerente herda da class Funcionario
+public class Gerente extends Funcionario{
+	
+	private int senha;
+	
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+//	public double getBonificacao() {
+//		return this.salario;
+//	}
+	
+}
