b8523f7b0fc5c9845187623ad30be753c27be16a
eliansamuel <eliansamuelsantos@gmail.com>
2021-11-09 17:56:37 -0300

    Using Composition to the Gerente, Administrador and Cliente classes

--- src/bytebank_herdado/Gerente.java
+++ src/bytebank_herdado/Gerente.java
@@ -3,7 +3,11 @@ package bytebank_herdado;
 // Gerente eh um Funcionario, Gerente herda da class Funcionario eh um Autenticavel
 public class Gerente extends Funcionario implements Autenticavel{
 	
-	private int senha;
+	private AutenticacaoUtil autenticador;
+
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
 	
 	public double getBonificacao() {
 		System.out.println("Chamando o método de bonificacao do Gerente");
@@ -12,16 +16,12 @@ public class Gerente extends Funcionario implements Autenticavel{
 
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 		
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
+		return this.autenticador.autentica(senha);
 	}
 }
