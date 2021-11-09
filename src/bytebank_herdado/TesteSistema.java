1933e4d64b70584e6048cef4cb5184ace8f66f43
eliansamuel <eliansamuelsantos@gmail.com>
2021-11-09 16:57:17 -0300

    Making the Autenticavel interface

---
+++ src/bytebank_herdado/TesteSistema.java
@@ -0,0 +1,18 @@
+package bytebank_herdado;
+
+public class TesteSistema {
+	
+	public static void main(String[] args) {
+		
+//		Gerente g1 = new Gerente();
+//		g1.setSenha(2222);
+//		
+//		Administrador adm1 = new Administrador();
+//		adm1.setSenha(3333);
+//		
+//		SistemaInterno si = new SistemaInterno();
+//		si.autentica(g1);
+//		si.autentica(adm1);
+	}
+
+}
